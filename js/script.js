4f1fc7bb5351edc304fa6fd48e13c59cff2c9427
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-20 13:50:13 +0100

    add smth

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function() {
+   console.log('dupa');
+    function randomString() {
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+        var str = '';
+        for (var i = 0; i < 10; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    }
+    function Column(name) {
+        var $column = $('<div>').addClass('column');
+        var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+        var $columnCardList = $('<ul>').addClass('column-card-list');
+        var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+        var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+        var self = this; // useful for nested functions
+    
+        this.id = randomString();
+        this.name = name;
+        this.$element = createColumn();
+    
+        function createColumn() {
+            // here is the code for creating the column, which you will find below
+        }
+      }
+
+
+})
